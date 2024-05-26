dd3ab86a9dfa90b9a4e70d19d49c3bf2adcc9941
Nik <nikitozer25@gmail.com>
2024-05-26 19:08:26 +0700

    CameraFollower

--- Assets/Scripts/Tools/CameraFollower.cs
+++ Assets/Scripts/Tools/CameraFollower.cs
@@ -3,6 +3,19 @@ using UnityEngine;
 public class CameraFollower : MonoBehaviour
 {
     [SerializeField] private Transform _target;
+    [SerializeField] private Vector3 _offcet;
+    [SerializeField] private float _cameraSpeed;
 
 
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var nextPosition = Vector3.Lerp(transform.position,_target.position + _offcet,_cameraSpeed * Time.fixedDeltaTime);
+
+        transform.position = nextPosition;
+    }
 }
