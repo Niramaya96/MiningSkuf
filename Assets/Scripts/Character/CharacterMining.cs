248f4d19de040a8306b58c3599aff5f705efd7a8
Nik <nikitozer25@gmail.com>
2024-06-01 12:39:07 +0700

    .

--- Assets/Scripts/Character/CharacterMining.cs
+++ Assets/Scripts/Character/CharacterMining.cs
@@ -2,19 +2,55 @@ using UnityEngine;
 
 public class CharacterMining : MonoBehaviour
 {
-    [SerializeField] private Vector3 _centrePosition;
-    [SerializeField] private float _sphereRadius;
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Vector3 _offset;
+    [SerializeField,Min(0f)] private float _sphereRadius;
     [SerializeField] private LayerMask _resourseLayer;
 
-    private Collider[] _hitColliders = new Collider[10];
-    private void TryMining()
-    {
-       var colliders = Physics.OverlapSphere(_centrePosition,_sphereRadius);
+    [Header("Gizmos")]
+    [SerializeField] private Color _gizmosColor;
 
-        foreach (var collider in colliders)
+    private readonly Collider[] _hitColliders = new Collider[10];
+    private int _hitCount;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+            TryGetResources();
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        var position = _startPoint.TransformPoint(_offset);
+        Gizmos.color = _gizmosColor;
+        Gizmos.DrawSphere(position,_sphereRadius);
+        
+    }
+    private void TryGetResources()
+    {
+       if (TryFindResources())
+       {
+            PerformMining();
+       }
+    }
+
+    private bool TryFindResources()
+    {
+        var position = _startPoint.TransformPoint(_offset);
+        _hitCount = Physics.OverlapSphereNonAlloc(position, _sphereRadius, _hitColliders, _resourseLayer.value);
+        return _hitCount > 0;
+    }
+    private void PerformMining()
+    {
+        for (int i = 0; i < _hitCount; i++)
         {
-            collider.TryGetComponent(out ResourceV2 resourse);
-            Debug.Log(resourse.Name);
+            if (_hitColliders[i].TryGetComponent(out ResourceV2 resource) == false)
+            {
+                Debug.Log("Не найдено");
+            }
+            
+            Debug.Log(resource.name);
         }
     }
 }
